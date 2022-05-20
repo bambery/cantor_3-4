7c6a9b346ca47884e5740bc120aa461b6ea47d36
bambery <lwszolek@gmail.com>
2022-05-20 17:16:35 -0500

    beginning to draw numberline

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const canvas = document.querySelector('.myCanvas');
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
-const ctx = canvas.getContext('2d');
-
-// make bg black
-ctx.fillStyle = 'rgb(0, 0, 0)';
-ctx.fillRect(0, 0, width, height);
-
-function degToRad(degrees) {
-    return degrees * Math.PI / 180;
-}
-
 const fractionPrototype = {
     num: 1,
     den: 1
@@ -19,11 +6,11 @@ const fractionPrototype = {
 function Fraction( numerator, denominator ) {
     this.num = numerator;
     this.den = denominator;
-}
+};
 
 function fractionToString() {
     return `${this.num}/${this.den}`;
-}
+};
 
 Fraction.prototype = fractionPrototype;
 Fraction.prototype.constructor = Fraction;
@@ -36,6 +23,8 @@ const lineSegmentPrototype = {
     size: new Fraction(1, 1)
 };
 
+// expects two fractions
+// returns a Line Segment
 function LineSegment( leftEndpoint,  rightEndpoint ) {
     this.left = leftEndpoint;
     this.right = rightEndpoint;
@@ -43,17 +32,18 @@ function LineSegment( leftEndpoint,  rightEndpoint ) {
     const length = subtractFrac( this.right, this.left);
     // "length" is a property on Array I don't want to shadow, using "size" for the size of the interval
     this.size = reduceFrac(length); 
-}
+};
 
 function lineSegmentToString() {
     return `Left: ${this.left.num}/${this.left.den}, Right: ${this.right.num}/${this.right.den}, Length: ${this.size}`;
-}
+};
 
 LineSegment.prototype = lineSegmentPrototype;
 LineSegment.prototype.constructor = LineSegment;
 LineSegment.prototype.toString = lineSegmentToString;
 
 // write a function for checking type via obj.__proto__.constructor.name
+// investigate new additions to js for null/undefined checks
 //function type(obj){
 //
 //}
@@ -65,7 +55,7 @@ function SegmentCollection(segments){
 
     this.size = function size(){
         var size = new Fraction(0, 1);
-        segments.forEach( segment =>{ size = addFrac(size, segment.size) }); 
+        this.segments.forEach( segment =>{ size = addFrac(size, segment.size) }); 
         size = reduceFrac(size);
         return size;
     };
@@ -76,15 +66,18 @@ function SegmentCollection(segments){
     };
 
     this.allGaps = function allGaps(){
-        segments.forEach( segment => { console.log(segment.toString()) });
+        this.segments.forEach( segment => { console.log(segment.toString()) });
         console.log(`Total gap size: ${this.gapSize()}.`);
     }
 
     this.push = function push(segmentArr){
-
         segmentArr.forEach( segment => { this.segments.push(segment) } );
     }
-}
+
+    this.smallestInterval = function smallestInterval() {
+        return this.segments[this.segments.length - 1].size.den;
+    }
+};
 
 // pass in the minuend first (the number to be subtracted from)
 function subtractFrac(left, right){
@@ -92,13 +85,13 @@ function subtractFrac(left, right){
     const denominator = left.den * right.den;
     // can/should I declare this return value const?
     return new Fraction(numerator, denominator); 
-}
+};
 
 function addFrac(left, right){
     const numerator = (left.num * right.den) + (left.den * right.num);
     const denominator = left.den * right.den;
     return new Fraction(numerator, denominator);
-}
+};
 
 function removeThird(segment) {
     //     LEFT           RIGHT
@@ -121,7 +114,7 @@ function removeThird(segment) {
     const rightSeg = new LineSegment( reduceFrac(rl), reduceFrac(rr) );
 
     return [leftSeg, rightSeg];
-}
+};
 
 function reduceFrac(frac){
     if (frac.num === 0 || frac.den === 0){
@@ -141,7 +134,7 @@ function reduceFrac(frac){
         y = mod;
     }
     return new Fraction( frac.num / x, frac.den / x );
-}
+};
 
 function cantor3_4(iterations) {
     if ( iterations < 1 ) {
@@ -159,20 +152,100 @@ function cantor3_4(iterations) {
         iterations = iterations - 1;
     }
     return myCol;
+};
+
+
+foo = cantor3_4(1);
+
+//////////////////////////////////////////////
+// https://bucephalus.org/text/CanvasHandbook/CanvasHandbook.html
+//////////////////////////////////////////////
+
+function fillCircle (contextObj, x, y, r) {
+    contextObj.beginPath();
+    contextObj.arc(x, y, r, 0, 2* Math.PI);
+    contextObj.fill();
+};
+
+// the x and y provided are the location of the dot under which the fraction will be displayed
+function drawFraction (ctx, frac, x, y){
+
+    ctx.textAlign = 'center';
+    ctx.lineWidth = fracBarWidth; 
+    ctx.textBaseline = endpointFontBaseline;
+    ctx.fillStyle = 'white';
+    
+    // draw numerator
+    let numTop = y + endpointFontTop;
+    ctx.fillText(frac.num, x, numTop);
+    // draw bar under numerator
+    let lineLen = Math.max( ctx.measureText(frac.num).width , ctx.measureText(frac.den).width ) ;
+    ctx.beginPath()
+    // what's that sneaky 0.5 at the end? It's so the line is crisp and white and not grey and fat
+    // https://bucephalus.org/text/CanvasHandbook/CanvasHandbook.html#linewidth
+    let barTop = numTop + fontSize + fracBarPad + 0.5;
+    ctx.moveTo( x - lineLen, barTop);
+    ctx.lineTo(x + lineLen, barTop);
+    ctx.stroke();
+    // draw denominator
+    let denTop = barTop + fracBarWidth + fracBarPad;
+    ctx.fillText(frac.den, x, denTop);
 }
 
-//foo = cantor3_4(3);
+const canvas = document.querySelector('.myCanvas');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
 
-/*
-const foo = new LineSegment(0, 1, 1, 1);
-const [fooL, fooR] = removeThird(foo);
-console.log("finished 1");
-const [barL, barR] = removeThird(fooL);
-console.log("finished 2");
-const [bazL, bazR] = removeThird(fooR);
-console.log("finished");
-const [mooL, mooR] = removeThird(barL);
-const [booL, booR] = removeThird(barR);
-const [shoeL, shoeR] = removeThird(bazL);
-const [newL, newR] = removeThird(bazR);
-*/
+const midH = Math.floor(height/2);
+const margin = 30;
+const dotSize = 5;
+const endpointFontTop = 15;
+const endpointFontBaseline = "top";
+const fontSize = 15;
+const numberlineWidth = 2.0;
+const intervalLineWidth = 2.3; 
+const fracBarPad = 2;
+const fracBarWidth = 1.0;
+
+// make bg black
+ctx.fillStyle = 'rgb(0, 0, 0)';
+ctx.fillRect(0, 0, width, height);
+
+// set some style defaults
+ctx.strokeStyle = '#ffffff';
+ctx.fillStyle = '#ffffff';
+ctx.font = `${fontSize}px Verdana`;
+
+// draw numberline
+ctx.lineWidth = numberlineWidth;
+ctx.beginPath();
+ctx.moveTo(margin, midH);
+ctx.lineTo(width - margin, midH);
+ctx.stroke();
+// draw endpoints and labels on the numberline
+fillCircle(ctx, margin, midH, dotSize);
+fillCircle(ctx, width-margin, midH, dotSize);
+//draw labels for endpoint
+zero = new Fraction(0,1);
+drawFraction(ctx, zero, margin, midH);
+one = new Fraction(1, 1);
+drawFraction(ctx, one, width-margin, midH);
+
+function drawPoints(ctx, segCol){
+
+    // for rendering this iteration, the smallest interval will determine the endpoints
+    const interval = foo.smallestInterval();
+    const intervalLength = width/interval;
+
+    commonDen = segCol.segments.map(segment => {
+        const Lmultiple = interval / segment.left.den;
+        const Rmultiple = interval / segment.right.den;
+        const newLeft = new Fraction(segment.left.num * Lmultiple, segment.left.den * Lmultiple);
+        const newRight = new Fraction(segment.right.num * Rmultiple, segment.right.den * Rmultiple);
+        return new LineSegment(newLeft, newRight); 
+    });
+    debugger;
+}
+
+drawPoints(ctx, foo);
